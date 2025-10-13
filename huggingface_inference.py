cec9569fd76ce577bfb4e6125adefbfb86f81c88
Songbo Hu <sh2091@cam.ac.uk>
2025-10-13 11:38:48 +0100

    upload_gelatod

--- huggingface_inference.py
+++ huggingface_inference.py
@@ -1,11 +1,13 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
 if __name__ == '__main__':
 
-    tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-small")
-    model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-small")
+    model_name = "google/flan-t5-small"
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 
-    input_ids = tokenizer.encode("Hi there! How are you?", return_tensors="pt")
-    output_ids = model.generate(input_ids, max_new_tokens=50, pad_token_id=tokenizer.eos_token_id)
+    prompt = "Translate to French: Cambridge students love gelato."
+    inputs = tokenizer(prompt, return_tensors="pt")
+    outputs = model.generate(**inputs, max_new_tokens=40)
 
-    print(tokenizer.decode(output_ids[:, input_ids.shape[-1]:][0], skip_special_tokens=True))
+    print(tokenizer.decode(outputs[0], skip_special_tokens=True))
